779edbe82c2bc2b4f2fd72a1959b02e185a81025
Naveen-Valmikam <naveen.valmikam@gmail.com>
2017-05-31 09:24:36 +1000

    Added utility method generateMessage

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+
+
 var publicpath = path.join(__dirname,'../public');
 const PORT = process.env.PORT || 3000;
 
@@ -12,34 +15,20 @@ var io = socketIO(server);
 app.use(express.static(publicpath));
 
   io.on('connection',(socket)=>{
-  console.log('New user connected');
+    console.log('New user connected');
 
-  socket.emit('newMessage',{
-    from:'Admin',
-    text:'Welcome to chat app',
-    createdAt:new Date().getTime()
-  });
-  socket.broadcast.emit('newMessage',{
-      from:'Admin',
-      text:'New User joined',
-      createdAt:new Date().getTime()
-  });
+    socket.emit('newMessage',generateMessage('Admin','Welcome to chat app'));
+    socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
-  socket.on('createMessage',(createMessage)=>{
-    io.emit('newMessage',{
-      from:createMessage.from,
-      text:createMessage.text,
-      createdAt:new Date().getTime()
+    socket.on('createMessage',(createMessage)=>{
+      io.emit('newMessage',generateMessage(createMessage.from,createMessage.text));
+      });
+
+    socket.on('disconnect',()=>{
+      console.log('User disconnected');
     });
   });
-
-  socket.on('disconnect',()=>{
-    console.log('User disconnected');
-  });
-
-
-});
-
+  
 server.listen(PORT,()=>{
   console.log(`Server started on port ${PORT}`);
 });
