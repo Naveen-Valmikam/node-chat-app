c07ebc8d65913f604ea515455ba804aa3fd93b91
Naveen-Valmikam <naveen.valmikam@gmail.com>
2017-06-01 15:31:53 +1000

    Add user list

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,21 +1,20 @@
 var socket = io();
 
 function scrollToBottom(){
+  // Selectors
   var messages = $('#messages');
   var newMessage = messages.children('li:last-child');
-
+  // Heights
   var clientHeight = messages.prop('clientHeight');
   var scrollTop = messages.prop('scrollTop');
   var scrollHeight = messages.prop('scrollHeight');
   var newMessageHeight = newMessage.innerHeight();
   var lastMessageHeight = newMessage.prev().innerHeight();
 
-
   if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
       messages.scrollTop(scrollHeight);
   };
 };
-
 
 socket.on('connect',function(){
   var params = $.deparam(window.location.search);
@@ -26,24 +25,33 @@ socket.on('connect',function(){
       window.location.href='/';
     }else {
       console.log('No error');
-    };
+    }
   });
-
 });
 
 socket.on('disconnect',function(){
   console.log('Disconnected from server');
 });
 
+socket.on('updateUserList',function(users){
+    var ol =$('<ol></ol>');
+
+    users.forEach(function(user){
+      ol.append($('<li></li>').text(user));
+    });
+
+    $('#users').html(ol);
+});
+
 socket.on('newMessage',function(message){
   var formattedTime = moment(message.createdAt).format('h:mm a');
-
   var template = $('#message-template').html();
   var html = Mustache.render(template,{
     text:message.text,
     from:message.from,
     createdAt:formattedTime
   });
+  
   $('#messages').append(html);
   scrollToBottom();
 });
@@ -72,7 +80,6 @@ $('#message-form').on('submit',function(e){
         messageTextBox.val('');
     });
 });
-
 
 var locationButton = $('#send-location');
 locationButton.on('click',function(){
--- server/server.js
+++ server/server.js
@@ -5,23 +5,31 @@ const socketIO = require('socket.io');
 
 const {generateMessage,generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
-var publicpath = path.join(__dirname,'../public');
+const publicpath = path.join(__dirname,'../public');
 const PORT = process.env.PORT || 3000;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
+
 app.use(express.static(publicpath));
 
   io.on('connection',(socket)=>{
+    console.log('New user connected');
 
     socket.on('join',(params,callback)=>{
         if(!isRealString(params.name) || !isRealString(params.room)){
-          callback('Name and room name are required');
+          return callback('Name and room name are required');
         }
 
         socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id,params.name,params.room);
+
+        io.to(params.room).emit('updateUserList',users.getUserList(params.room));
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
         socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined.`));
         callback();
     });
@@ -36,7 +44,11 @@ app.use(express.static(publicpath));
       });
 
     socket.on('disconnect',()=>{
-      console.log('User disconnected');
+      var user = users.removeUser(socket.id);
+      if(user){
+        io.to(user.room).emit('updateUserList',users.getUserList(user.room));
+        io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left the ${user.room} room.`));
+      }
     });
   });
 
